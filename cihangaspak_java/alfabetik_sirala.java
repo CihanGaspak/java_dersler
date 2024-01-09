0eb1f2c0b11a19b5c294522f3a18f1bff28de017
CihanGaspak <cihangir2344@gmail.com>
2024-01-09 10:26:21 +0300

    Girilen kelime/sehirleri afabetik sıraya göre soyleyen program

---
+++ cihangaspak_java/alfabetik_sirala.java
@@ -0,0 +1,29 @@
+package java_dersler.cihangaspak_java;
+import java.util.Scanner;
+
+
+public class alfabetik_sirala {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("1. şehri giriniz: ");
+        String sehir1 = input.next();
+
+        System.out.print("2. şehri giriniz: ");
+        String sehir2 = input.next();
+
+        if(sehir1.compareToIgnoreCase(sehir2)<0) {
+            System.out.println(sehir1+", "+sehir2+"'den önce gelir.");
+        }
+        else if(sehir1.compareToIgnoreCase(sehir2)==0){
+            System.out.println(sehir1+", "+sehir2+" ile ayni siradadir.");
+        }
+        else {
+            System.out.println(sehir2+", "+sehir1+"'den önce gelir.");
+        }
+    input.close();
+    }
+}
