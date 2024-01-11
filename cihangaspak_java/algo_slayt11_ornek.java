650f4dfbc24fc6e8b623d41a0b3e3b535d74b39d
CihanGaspak <cihangir2344@gmail.com>
2024-01-11 11:11:34 +0300

    txt dosyasından okuyp yazdırma işlemini yapan kodlar

--- cihangaspak_java/algo_slayt11_ornek.java
+++ cihangaspak_java/algo_slayt11_ornek.java
@@ -1,5 +1,13 @@
 package java_dersler.cihangaspak_java;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+
 public class algo_slayt11_ornek {
     public static void main(String[] args) {
         //10.000 adet mesaj yazan uygulama for ile
@@ -25,7 +33,7 @@ public class algo_slayt11_ornek {
         */
         
         //10.000 mesajı tek print ile yazdır
-        String mesaj ;
+        /*String mesaj ;
         for(int i =1; i<=10000; i++){
             if(i%2==1) 
             mesaj = "Yazilim Muhendisligi";
@@ -33,6 +41,30 @@ public class algo_slayt11_ornek {
             mesaj = "Cok calismaliyiz cokk";
 
             System.out.println(mesaj+" "+i);            
+        }*/
+        ArrayList<String> ad_soyad = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("ad_soyad.txt"))) {
+            String line;
+            int arananIndex;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                arananIndex = line.indexOf(" ");
+                if(arananIndex != -1) {
+                    ad_soyad.add("SAYIN " + line.substring(0, arananIndex).toUpperCase() + " " + line.substring(arananIndex + 1).toUpperCase());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("ad_soyad_sayin.txt"))) {
+            for(String sayin_ad_soyad: ad_soyad){
+                writer.write(sayin_ad_soyad);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
