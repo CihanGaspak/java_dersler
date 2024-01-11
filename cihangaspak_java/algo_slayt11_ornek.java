54f52c33e6acdf8fdd0804901aaf16676e748797
CihanGaspak <cihangir2344@gmail.com>
2024-01-11 14:15:32 +0300

    11.slayt son odev döndü ile tablo ind.i ve top. yazdırma

--- cihangaspak_java/algo_slayt11_ornek.java
+++ cihangaspak_java/algo_slayt11_ornek.java
@@ -148,7 +148,7 @@ public class algo_slayt11_ornek {
         
         //ÇARPIM TABLOSU YAZDIR
         
-        //baslık
+        /*//baslık
         System.out.println("                    Carpim Tablosu");
         //sayı baslıgı
         System.out.print("    ");
@@ -164,7 +164,29 @@ public class algo_slayt11_ornek {
                 System.out.printf("%4d",i*j);
             }
             System.out.println();
-        }
+        }*/
 
+        //AZALAN SAYILARI ÜÇGENSEL YAZDIR
+        /*for(int i =1; i<8;i++){
+            for(int j =i; j>0; j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        } */ 
+
+        //11.slayt son ödev 
+        System.out.println("              Tablo Indisleri ve Indis Toplamlari");
+        System.out.print("  | ");
+        for(int i=1; i<10;i++){
+            System.out.printf("%6d",i);
+        }
+        System.out.println("\n--|----------------------------------------------------------------");
+        for(int i=1; i<10;i++){
+            System.out.print(i+" |  ");
+            for(int j =1;j<10;j++){
+                System.out.printf("%s%s-%2d  ",j,i,(i+j));
+            }
+            System.out.println();
+        }
     }
 }
