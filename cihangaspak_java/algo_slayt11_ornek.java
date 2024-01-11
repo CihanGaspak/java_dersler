5a83e9137be915d8c364d0efead521ea2c27e902
CihanGaspak <cihangir2344@gmail.com>
2024-01-11 10:33:41 +0300

    while ile yapılan odev

--- cihangaspak_java/algo_slayt11_ornek.java
+++ cihangaspak_java/algo_slayt11_ornek.java
@@ -2,6 +2,16 @@ package java_dersler.cihangaspak_java;
 
 public class algo_slayt11_ornek {
     public static void main(String[] args) {
-        
+        //10.000 adet mesaj yazan uygulama for ile
+        for(int i =1; i<=10000; i++){
+            System.out.println("Merhaba "+i+".kez");
+        }
+
+        //10.000 adet mesaj yazan uygulama while ile
+        int i = 1;
+        while (i<=10000){
+            System.out.println("Merhaba "+i+".kez");
+            i++;
+        }
     }
 }
