b56335ed5d37442b3a4576b3207740a439af811c
CihanGaspak <cihangir2344@gmail.com>
2024-01-15 11:57:35 +0300

    ymh22 but soru 4

--- cihangaspak_java/ymh22but.java
+++ cihangaspak_java/ymh22but.java
@@ -30,7 +30,7 @@ public class ymh22but {
             }
         }*/
 
-        //Soru 3 
+        /*//Soru 3 
         char a = '0', b = '1';
         int c = (int)b, d = (int)a;
         System.out.println(a + ":" + c);
@@ -39,11 +39,35 @@ public class ymh22but {
         System.out.println((char)c + ":" + b);
         System.out.println(a + "--" + b);
         //çıktısı nedir?
-        
+
         //0:49
         //1:48
         //0:0--1:1
         //0--1
+        */
+
+        //Soru 4 
+        int[] sayilar = {5,1,4,2,3,88,9};
+        int enkInd, n = sayilar.length;
+        int enk, temp;
+
+        for(int i=0; i<sayilar.length; i++){
+            enk = sayilar[i];
+            enkInd = i;
+            for(int j=i; j<n; j++){
+                if(sayilar[j]<enk){
+                    enk = sayilar[j];
+                    enkInd = j; 
+                }
+            }
+            temp = sayilar[i];
+            sayilar[i] = sayilar[enkInd];
+            sayilar[enkInd] = temp;
+        }
+
+        for(int i=0; i<sayilar.length; i++){
+            System.out.print(sayilar[i]+ " ");
+        }
 
     }
 }
